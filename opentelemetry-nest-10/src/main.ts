7dd1e24cfa832f7ce9c89cc142d9cc47d8d5a8fe
EnoughThinking <justinsetonpro1994@gmail.com>
2023-12-19 12:52:38 +0500

    chore: disable tracing and update nest version

--- opentelemetry-nest-10/src/main.ts
+++ opentelemetry-nest-10/src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  require('./tracing');
+  // require('./tracing');
   const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   app.useLogger(app.get(Logger));
-  app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('Nest 10')
